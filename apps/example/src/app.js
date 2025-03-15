dfaae58d30a4f9743589bd187047f0c39e8513b7
Simon Emms <simon@simonemms.com>
2025-03-15 14:12:01 +0000

    more stuff

--- apps/example/src/app.js
+++ apps/example/src/app.js
@@ -1,19 +1,46 @@
 import express from "express";
-import passport from "passport";
-import expressSession from "express-session";
+// import passport from "passport";
+// import expressSession from "express-session";
 import { BasicStrategy } from "passport-http";
 
 const app = express();
 
-app.use(
-  expressSession({
-    secret: "q1w2e3r4",
-  }),
-);
-app.use(passport.initialize());
-app.use(passport.session());
-passport.use(
-  new BasicStrategy((username, password, done) => {
+// app.use(
+//   expressSession({
+//     secret: "q1w2e3r4",
+//   }),
+// );
+// app.use(passport.initialize());
+// app.use(passport.session());
+// passport.use(
+//   new BasicStrategy((username, password, done) => {
+//     done(null, {
+//       username,
+//       password,
+//     });
+//     // console.log({
+//     //   username,
+//     //   password,
+//     //   done,
+//     // });
+//   }),
+// );
+
+app.get("/", (req, res, next) => {
+  const req2 = {
+    url: req.url,
+    body: req.body ?? {},
+    headers: req.headers,
+
+    body: { ...req.body },
+    headers: { ...req.headers },
+    params: { ...req.params },
+    query: { ...req.params },
+    method: req.method,
+    url: req.url,
+  };
+
+  const strategy = new BasicStrategy((username, password, done) => {
     done(null, {
       username,
       password,
@@ -23,31 +50,47 @@ passport.use(
     //   password,
     //   done,
     // });
-  }),
-);
+  });
 
-app.get(
-  "/",
-  (req, res, next) => {
-    const req2 = {
-      url: req.url,
-      body: req.body ?? {},
-      headers: req.headers,
-
-      body: { ...req.body },
-      headers: { ...req.headers },
-      params: { ...req.params },
-      query: { ...req.params },
-      method: req.method,
-      url: req.url,
-    };
-    passport.authenticate("basic", { session: false })(req2, res, next);
-  },
-  (req, res) => {
-    res.json(req.user);
-  },
-);
+  PassportSDK.authenticate(strategy, { session: false })(req2, res, () => {
+    console.log(222);
+    res.json(req2.user);
+  });
+});
 
 app.listen(3000, () => {
   console.log("Listening on 3000");
 });
+
+class PassportSDK {
+  error(err) {
+    console.log({
+      err,
+    });
+  }
+
+  fail(challenge, status) {
+    console.log({
+      challenge,
+      status,
+    });
+  }
+
+  pass() {
+    console.log("pass");
+  }
+
+  redirect(url, status = undefined) {
+    console.log({ url, status });
+  }
+
+  success(user, info = undefined) {
+    console.log({ user, info });
+  }
+
+  static authenticate(strategy, opts) {
+    return (req, res, done) => {
+      done(null, { hello: "twatty" });
+    };
+  }
+}
